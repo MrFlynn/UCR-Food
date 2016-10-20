8576516cf47f69bc3b3d554a59ea28e96ba78e15
MrFlynn <nick@pleatsikas.me>
2016-10-19 22:07:12 -0700

    Added quick config parsing algorithm. Included in requirements.txt.

---
+++ ucrfood/config.py
@@ -0,0 +1,24 @@
+import configparser
+import os.path as path
+
+
+class Config(object):
+    def __init__(self, filename):
+        self.config_file = path.abspath(path.join('../config', filename))
+        self.config = configparser.ConfigParser()
+
+    def get_config(self):
+        self.config.read(self.config_file)
+
+    def construct_url(self):
+        urls = []
+
+        for section in self.config.sections()
+            if section == 'DEFAULT':
+                continue
+            else:
+                url_base = self.config['DEFAULT']['BaseURL']
+                location_num = self.config[section]['LocationNum']
+                location_name = self.config[section]['LocationName']
+
+                return urls.append(url_base + location_num + location_name)
