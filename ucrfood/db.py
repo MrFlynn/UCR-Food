187e86b99da6cb51ac82a85c749f05051ed21e3d
MrFlynn <nick@pleatsikas.me>
2016-12-09 23:13:52 -0800

    Added RethinkDB methods and configuration stub.

---
+++ ucrfood/db.py
@@ -0,0 +1,59 @@
+import rethinkdb as rdb
+from datetime import datetime, timedelta
+
+
+class Database(object):
+    def __init__(self, host: str, port: int, db_name: str, db_username: str, db_password: str = None):
+        self.host = host
+        self.port = port
+        self.db_name = db_name
+        self.db_username = db_username
+
+        # Optional auth arguments
+        self.db_password = db_password
+
+        # Connection
+        self.conn = None
+        self.connect()
+
+    def connect(self):
+        """
+        Connects to RethinkDB server. If no database password is provided, then connect without
+        authentication.
+        """
+
+        if not self.db_password:
+            self.conn = rdb.connect(self.host,
+                                    self.port,
+                                    self.db_name,
+                                    self.db_username)
+        else:
+            self.conn = rdb.connect(self.host,
+                                    self.port,
+                                    self.db_name,
+                                    self.db_username,
+                                    self.db_password)
+
+    def add_menu_data(self, menu):
+        """
+        Method for inserting menu into 'menus' table.
+        """
+
+        rdb.table('menus').insert(menu).run(self.conn)
+
+    def update_menu_on_date(self, date: str, menu: str):
+        """
+        Replace menu for specific date in 'menus' table.
+        """
+
+        rdb.table('menus').filter({'menu_date': date}).replace(menu).run(self.conn)
+
+    def get_menu_with_duration(self, day_delta: int):
+        """
+        Returns all table entries from the current date to n days from now.
+        """
+
+        start_date = str(datetime.now().date())
+        end_date = str(datetime.now().date() + timedelta(days=day_delta))
+
+        rdb.table('menu').between({'menu_date': start_date}, {'menu_date': end_date}).run(self.conn)
