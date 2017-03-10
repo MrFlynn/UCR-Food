e24a51fd507bdfbbda13c0fb4433bf22f62cdb54
MrFlynn <nick@pleatsikas.me>
2017-03-09 21:16:43 -0800

    Reorganized main to accommodate future scheduling feature.

--- main.py
+++ main.py
@@ -6,7 +6,24 @@ from urllib.parse import quote_plus
 from multiprocessing import Pool
 
 
-def main():
+"""
+Global variables for main function:
+"""
+
+# Sets database variable to null.
+app_db = None
+
+# Bases for urls to use during runtime.
+base_urls = []
+
+
+def _construct():
+    """
+    Description: runs when application is first starts up. Opens configuration
+    files, constructs the list of urls from which to pull data, and then
+    build the database connection.
+    """
+
     # Location configuration
     location_config = ucrfood.Config('location.ini')
     location_config.construct_dict(check_params=['BaseURL',
@@ -21,9 +38,7 @@ def main():
                                            'Port',
                                            'DBPassword'])
 
-    # Generate and list URLS:
-    base_urls = []
-
+    # Construct base urls
     for key in location_config.config_dict:
         if key == 'MAIN':
             continue
@@ -41,7 +56,7 @@ def main():
 
             base_urls.append(full_url)
 
-    # Rethink config details:
+    # RethinkDB connection details:
     db_init_args = (db_config.config_dict.get('CONNECTION').get('host'),
                     db_config.config_dict.get('CONNECTION').get('port'),
                     db_config.config_dict.get('DB_INFO').get('dbname'),
@@ -49,33 +64,44 @@ def main():
                     db_config.config_dict.get('AUTH').get('dbpassword'))
 
     # Initialize database connection:
-    rd_database = ucrfood.Database(*db_init_args)
+    app_db = ucrfood.Database(*db_init_args)
 
-    # List of base_urls with all the proper dates parametrized.
-    complete_urls = []
+
+def main():
+    """
+    Builds all urls with date parameters, grabs data from the site, and
+    then uploads all data to the database.
+
+    todo: Figure out HTMLElement pickling problem.
+    """
+
+    # Urls with date objects:
+    runtime_urls = []
 
     # Get menus for the next two weeks and push to database:
     for url in base_urls:
         for i in range(15):
             # Get run date + timedelta:
-            current_date = (datetime.now().date()
-                            + timedelta(days=i)).strftime('%m/%d/%Y')
+            current_date = quote_plus(datetime.now().date() +
+                                      timedelta(days=i)).strftime('%m/%d/%Y')
 
             # Construct the url with the new date:
             current_url = '{base}&dtdate={date}'.format(base=url,
-                                                        date=quote_plus(current_date))
+                                                        date=current_date)
 
-            complete_urls.append(current_url)
+            runtime_urls.append(current_url)
 
     with Pool(processes=4) as pool:
         # Grab and sort the data from each url:
-
-        results = [pool.apply_async(ucrfood.FoodSort, (i, )) for i in complete_urls]
+        results = [pool.apply_async(ucrfood.FoodSort, (i, )) for i in runtime_urls]
 
         # Push to the database:
         for item in results:
-            rd_database.add_menu_data(item.get().tree_data)
+            app_db.add_menu_data(item.get().tree_data)
 
 # Run the application:
 if __name__ == '__main__':
+    # todo: have main function run within a scheduler.
+
+    _construct()
     main()
