a2891913b645a1ad35d958ecae39ea8327b18667
MrFlynn <nick@pleatsikas.me>
2017-11-10 01:18:54 -0800

    Moved Makefile to virtualenv folder.

--- virtualenv/venv_installed.sh
+++ virtualenv/venv_installed.sh
@@ -6,7 +6,7 @@ if [[ $(python3 -m pip freeze) != *virtualenv* ]]; then
 fi
 
 echo -e "--> Virtualenv installed sucessfully. Creating environment..."
-virtualenv -p $(which python3) ./venv
+virtualenv -p $(which python3) ../venv
 
 echo -e "--> Environment created. Enter with \`source venv/bin/activate\`"
 
