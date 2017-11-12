3bc1797fc4b7367fee1d554d27339ff51f17183f
MrFlynn <nick@pleatsikas.me>
2017-11-12 14:32:56 -0800

    Added error catching for keyboard interrupt and system exit.

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
 import os
-from FoodService import FoodService
+from food_service import FoodService
 
 # Application entrypoint:
 if __name__ == '__main__':
     app = FoodService(url_conf=os.path.abspath('./config/location.ini'),
                       db_conf=os.path.abspath('./config/db.ini'))
 
-    # Run the application.
-    app.run()
+    try:
+        # Run every 24 hours.
+        app.run_schedule(interval=24)
+    except KeyboardInterrupt or SystemExit:
+        print("Stopping...")
+        app.stop()
